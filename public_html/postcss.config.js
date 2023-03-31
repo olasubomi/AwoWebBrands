a4d51989add67709297f141121ab7ae951ebe912
Maroprecious <maroumukoro98@gmail.com>
2023-03-31 04:58:56 -0700

    final commit

--- public_html/postcss.config.js
+++
@@ -1,9 +0,0 @@
-import postcss from 'postcss';
-import cssnano from 'cssnano';
-module.exports = {
-    plugins: [
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ],
-};
